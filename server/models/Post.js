582a881928c4a551b702add111fd68ad5aad17fc
Sam <bills2021e@gmail.com>
2023-09-02 21:14:14 -0400

    Update post model to reflect potential content

--- server/models/Post.js
+++ server/models/Post.js
@@ -4,6 +4,11 @@ const { Schema } = mongoose;
 
 const PostModel = new Schema({
   user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+  content: {
+    text: { type: String, required: false },
+    image: { data: Buffer, contentType: String, required: false },
+    ref: { type: mongoose.Types.ObjectId, ref: "Post", required: false },
+  },
   comments: { type: [ mongoose.Types.ObjectId ], ref: "Comment" },
   upVotes: { type: Number, default: 0 },
   time: { type: Date, required: true }
